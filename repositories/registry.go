a44a6c2665fad7cf442906523b09b9075f3ae5ae
Abdul Latif Munjiat <abdul23lm@gmail.com>
2025-02-18 00:25:09 +0700

    feat: implement user repository methods, error handling, and environment binding

---
+++ repositories/registry.go
@@ -0,0 +1,21 @@
+package repositories
+
+import (
+	repositories "conest-user-svc/repositories/user"
+	"gorm.io/gorm"
+)
+
+type Registry struct {
+	db *gorm.DB
+}
+type IRepositoryRegistry interface {
+	GetUser() repositories.IUserRepository
+}
+
+func NewRepositoryRegistry(db *gorm.DB) IRepositoryRegistry {
+	return &Registry{db: db}
+}
+
+func (r *Registry) GetUser() repositories.IUserRepository {
+	return repositories.NewUserRepository(r.db)
+}
